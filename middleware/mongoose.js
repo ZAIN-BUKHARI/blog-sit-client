2b77d775eede1e3b86c8ee7e6d259ad1961c4ac5
ZAIN-BUKHARI <zainnnnnnnnnnnnnnnnnnnn@gmail.com>
2023-06-13 10:47:43 -0700

    15 mongouri

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     // await mongoose.connect('mongodb://localhost:27017/Blog');
-    await mongoose.connect('mongodb+srv://123:123@e-store.uf5qztz.mongodb.net/blogs=true&w=majority');
+    await mongoose.connect(process.env.MONGO);
   } catch (error) {
     throw new Error("Connection failed!");
   }
