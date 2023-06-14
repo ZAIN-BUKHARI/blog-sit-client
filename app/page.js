25bafd15ce534c14409c450eea92b69cff0e3329
ZAIN-BUKHARI <zainnnnnnnnnnnnnnnnnnnn@gmail.com>
2023-06-14 07:06:59 -0700

    22 responsive

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Feature from '../components/feature/Feature'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Snack from "@/components/Snackbar/Snack";
+import style from './page.module.css'
 
 async function getData() {
   const res = await fetch("http://localhost:3000/api/blog", {
@@ -21,8 +22,10 @@ async function getData() {
   return (
     <>
     <Snack />
+    <div className={style.container}>
     <Feature blogs={blogs} />
     <Signleblog blogs={blogs}/>
+    </div>
     </>
   );
 }
